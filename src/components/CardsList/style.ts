0e11eb91014f2634749b3d80c7dc1ab2703d5141
Anderson Keller <andersonkellervendas@hotmail.com>
2022-12-27 00:11:41 -0300

    fix: animation CardList

--- src/components/CardsList/style.ts
+++ src/components/CardsList/style.ts
@@ -9,4 +9,19 @@ export const StyledList = styled.ul`
   width: 100vw;
   max-width: 1100px;
   margin: 0 auto;
+  height: 400px;
+  animation: listAnimation 0.5s ease;
+
+  @keyframes listAnimation {
+    0% {
+      opacity: 0.1;
+    }
+    50% {
+      opacity: 0.6;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
 `;
