b02bad73baeb8c4634bc5af9abc3a7540ae12571
Anderson Keller <andersonkellervendas@hotmail.com>
2022-12-28 12:09:36 -0300

    define register Login

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -6,9 +6,16 @@ import { StyledForm, StyledInput } from "./style";
 export interface iLoginFormProps {
   showLogin: boolean;
   setShowLogin: (showLogim: boolean) => void;
+  onSubmit: () => void;
+  register: {};
 }
 
-export function LoginForm({ showLogin, setShowLogin }: iLoginFormProps) {
+export function LoginForm({
+  showLogin,
+  setShowLogin,
+  register,
+  onSubmit,
+}: iLoginFormProps) {
   const navigate = useNavigate();
   function closeModal() {
     setShowLogin(!showLogin);
@@ -17,13 +24,13 @@ export function LoginForm({ showLogin, setShowLogin }: iLoginFormProps) {
   return (
     <StyledModalWrapper>
       {" "}
-      <StyledForm>
+      <StyledForm onSubmit={onSubmit}>
         <button className="closeButton" onClick={closeModal}>
           X
         </button>
         <h2>Faça Login</h2>
-        <StyledInput placeholder="Digite seu email" />
-        <StyledInput placeholder="Digite sua senha" />
+        <StyledInput {...register} placeholder="Digite seu email" />
+        <StyledInput {...register} placeholder="Digite sua senha" />
         <StyledButton type="submit">Login</StyledButton>
       </StyledForm>
     </StyledModalWrapper>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,56 @@
 import { useContext } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { LoginForm } from "../../components/LoginForm/LoginForm";
 import { UserContext } from "../../context/UserContext/UserContext";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { StyledModalWrapper } from "../CardPage/style";
+import { apiAuth } from "../../service/api";
 
 export function Login() {
   const { showLogin, setShowLogin } = useContext(UserContext);
+  type iFormData = {
+    name: string;
+    email: string;
+    password: string;
+  };
+  const loginSchema = yup.object().shape({
+    name: yup
+      .string()
+      .required("Nome obrigatório")
+      .min(3, "no mínimo 3 caracteres"),
+    email: yup.string().required("Email obrigatório").email("formato inválido"),
+    password: yup
+      .string()
+      .required("Senha obrigatória")
+      .min(6, "deve conter ao menos 6 caracteres"),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<iFormData>({ resolver: yupResolver(loginSchema) });
+  const onSubmit: SubmitHandler<iFormData> = async (data: iFormData) =>
+    onSubmitApi(data);
+  function onSubmitApi(data: iFormData) {
+    async function loginApi() {
+      try {
+        const res = await apiAuth.post("/login", data);
+        console.log(res);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    loginApi();
+  }
   return (
     <StyledModalWrapper>
-      <LoginForm showLogin={showLogin} setShowLogin={setShowLogin} />
+      <LoginForm
+        onSubmit={handleSubmit(onSubmitApi)}
+        register={register}
+        showLogin={showLogin}
+        setShowLogin={setShowLogin}
+      />
     </StyledModalWrapper>
   );
 }
